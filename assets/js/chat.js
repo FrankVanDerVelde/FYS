752ec92673d2cb204499ac78a7ed379a1f87fcdc
veldef1 <frank.van.der.velde@hva.nl>
2021-10-06 17:27:32 +0200

    Pushing profile beginnings

---
+++ assets/js/chat.js
@@ -0,0 +1,25 @@
+const showChatBtn = document.querySelector('#chatBtn');
+const chatBox = document.querySelector('#main-chat-box');
+const closeChatBtn = document.querySelector('#close-chat');
+
+showChatBtn.addEventListener('click', () => {
+    chatBox.classList.remove('hide-chat');
+    chatBox.classList.add('show-chat');
+});
+
+closeChatBtn.addEventListener('click', () => {
+    chatBox.classList.remove('show-chat');
+    chatBox.classList.add('hide-chat');
+});
+
+const checkdevice =  () => {
+    const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+    if (isMobile) {
+        chatBox.style.width = "100%";
+    } else {
+        chatBox.style.width = "360px";
+    }
+}
+
+checkdevice();
+window.addEventListener('resize', checkdevice);
