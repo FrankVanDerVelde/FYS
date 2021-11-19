5c65cd3bce90d60d0bf1e6b72ab8854a666dce9d
Dinny van Huizen <dinnyvanhuizen@gmail.com>
2021-11-19 10:29:39 +0100

    fixed bug with selection, added darkmode switch to filters.

--- wwwroot/assets/js/matches/filters.js
+++ wwwroot/assets/js/matches/filters.js
@@ -1,9 +1,8 @@
-function changeFilter(item)
-{
+function changeFilter(item) {
 
     console.log(item.className);
     var result;
-
+    var wantToRemove = false;
     const parentDiv = document.getElementsByClassName('toegepaste-filters')[0];
     console.log(parentDiv);
 
@@ -11,24 +10,23 @@ function changeFilter(item)
     for (let i = 0; i < parentDiv.children.length; i++) {
 
         let text = item.className.toLowerCase();
-        text = text.slice(6,text.length);
-        result = text.includes( parentDiv.children[i].id.toLowerCase());
-
-        if(item.innerHTML === parentDiv.children[i].innerText)
-        {
+        text = text.slice(6, text.length);
+        result = text.includes(parentDiv.children[i].id.toLowerCase());
+        
+        
+        if (item.innerHTML === parentDiv.children[i].innerText) {
             deleteFilter(parentDiv.children[i].id);
+            wantToRemove = true;
+        } else if (result) {
+            console.log( parentDiv.children[i]);
+            parentDiv.children[i].children[0].innerHTML = item.innerHTML;
 
         }
-        if(result)
-        {
-            parentDiv.children[i].innerHTML = item.innerHTML;
 
-        }
     }
-    if(!result)
-    {
-        switch (item.className)
-        {
+    if (result === false || parentDiv.children.length === 0 && wantToRemove === false) {
+
+        switch (item.className) {
             case "optionGeslacht":
 
                 createFilter("geslacht", item.innerHTML);
@@ -49,35 +47,34 @@ function changeFilter(item)
                 break;
         }
     }
+
 }
 
 
-function createFilter(typeFilter, valueFilter)
-{
+function createFilter(typeFilter, valueFilter) {
 
     var parentDiv = document.createElement("div");
     parentDiv.className = "gekozen";
     parentDiv.id = typeFilter;
     var childDiv = document.createElement("p");
     childDiv.className = "filterOption";
-    childDiv.id = "option"+typeFilter;
+    childDiv.id = "option" + typeFilter;
 
     childDiv.innerHTML = valueFilter;
     parentDiv.appendChild(childDiv);
 
 
     document.getElementsByClassName("toegepaste-filters")[0].appendChild(parentDiv);
-
+    console.log("created");
 }
 
-function deleteFilter(itemToRemove)
-{
+function deleteFilter(itemToRemove) {
 
     var parentDiv = document.getElementsByClassName("toegepaste-filters")[0];
     var childToRemove = document.getElementById(itemToRemove);
     console.log(childToRemove);
     parentDiv.removeChild(childToRemove);
 
-    console.log("test");
+    console.log("removed");
 
 }
