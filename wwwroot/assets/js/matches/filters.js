edc8566ae5b65934e5c9145d1b88c4033dc90905
Dinny van Huizen <dinnyvanhuizen@gmail.com>
2021-11-19 19:41:17 +0100

    fixed bug with filters

--- wwwroot/assets/js/matches/filters.js
+++ wwwroot/assets/js/matches/filters.js
@@ -1,25 +1,23 @@
 function changeFilter(item) {
 
-    console.log(item.className);
     var result;
     var wantToRemove = false;
     const parentDiv = document.getElementsByClassName('toegepaste-filters')[0];
-    console.log(parentDiv);
-
 
     for (let i = 0; i < parentDiv.children.length; i++) {
 
         let text = item.className.toLowerCase();
         text = text.slice(6, text.length);
         result = text.includes(parentDiv.children[i].id.toLowerCase());
-        
-        
+
         if (item.innerHTML === parentDiv.children[i].innerText) {
             deleteFilter(parentDiv.children[i].id);
             wantToRemove = true;
-        } else if (result) {
-            console.log( parentDiv.children[i]);
+            break;
+        }
+        if (result) {
             parentDiv.children[i].children[0].innerHTML = item.innerHTML;
+            break;
 
         }
 
@@ -27,22 +25,22 @@ function changeFilter(item) {
     if (result === false || parentDiv.children.length === 0 && wantToRemove === false) {
 
         switch (item.className) {
-            case "optionGeslacht":
+            case "optionGender":
 
-                createFilter("geslacht", item.innerHTML);
-
-                break;
-            case "optionRadius":
-                createFilter("radius", item.innerHTML);
+                createFilter("gender", item.innerHTML);
 
                 break;
-            case "optionBestemming":
-                createFilter("bestemming", item.innerHTML);
+            case "optionDistance":
+                createFilter("distance", item.innerHTML);
 
                 break;
-            case "optionLeeftijd":
+            case "optionDestination":
+                createFilter("destination", item.innerHTML);
 
-                createFilter("leeftijd", item.innerHTML);
+                break;
+            case "optionAge":
+
+                createFilter("age", item.innerHTML);
 
                 break;
         }
@@ -54,7 +52,7 @@ function changeFilter(item) {
 function createFilter(typeFilter, valueFilter) {
 
     var parentDiv = document.createElement("div");
-    parentDiv.className = "gekozen";
+    parentDiv.className = "chose";
     parentDiv.id = typeFilter;
     var childDiv = document.createElement("p");
     childDiv.className = "filterOption";
@@ -65,14 +63,13 @@ function createFilter(typeFilter, valueFilter) {
 
 
     document.getElementsByClassName("toegepaste-filters")[0].appendChild(parentDiv);
-    console.log("created");
+    console.log("added");
 }
 
 function deleteFilter(itemToRemove) {
 
     var parentDiv = document.getElementsByClassName("toegepaste-filters")[0];
     var childToRemove = document.getElementById(itemToRemove);
-    console.log(childToRemove);
     parentDiv.removeChild(childToRemove);
 
     console.log("removed");
