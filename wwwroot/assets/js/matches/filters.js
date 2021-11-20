e335315367e1ea9f0e829f148d10f25133fa63fe
Dinny van Huizen <dinnyvanhuizen@gmail.com>
2021-11-20 17:33:21 +0100

    dynamically add interest filters, ONLY HEAD CAT.

--- wwwroot/assets/js/matches/filters.js
+++ wwwroot/assets/js/matches/filters.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", async function (){
 
 
   try{
-      var interests = await FYSCloud.API.queryDatabase("SELECT * FROM fys_is109_4_harmohat_chattest.interestscategory; ")
-        console.log(interests);
+      fillInterestFilter();
   }
   catch(e){
         console.error(e);
@@ -11,39 +10,80 @@ document.addEventListener("DOMContentLoaded", async function (){
 
 })
 
+var amountOfInterestFilters = 0;
+
+async function fillInterestFilter()
+{
+    var interests = await FYSCloud.API.queryDatabase("SELECT * FROM fys_is109_4_harmohat_chattest.interestscategory; ")
+    await createFilterOption(interests);
+    console.log(interests);
+}
+
+async function createFilterOption(interests)
+{
+    const parentDiv = document.getElementsByClassName('dropdown-content-filter')[0]; //<-- first dro
 
 
+    for (let i = 0; i < interests.length ; i++) {
+        var childdiv = document.createElement("a");
+        childdiv.setAttribute("class", "optionInterest");
+        childdiv.setAttribute("href",  "javascript:void(0)") ;
+        childdiv.setAttribute("onclick", "changeFilter(this)");
+        childdiv.setAttribute("id", interests[i].description);
 
+        childdiv.innerHTML =  interests[i].description;
+        parentDiv.appendChild(childdiv);
 
+    }
 
+}
 
+async function changeFilter(item) {
 
-function changeFilter(item) {
-
-    var result;
+    var exists;
     var wantToRemove = false;
     const parentDiv = document.getElementsByClassName('toegepaste-filters')[0];
-
+    // get all active filters
     for (let i = 0; i < parentDiv.children.length; i++) {
 
-        let text = item.className.toLowerCase();
-        text = text.slice(6, text.length);
-        result = text.includes(parentDiv.children[i].id.toLowerCase());
+        //put classname as string and cut "Option" from classname.
+        let classname = item.className.toLowerCase();
+        classname = classname.slice(6, classname.length);
+        exists = classname.includes(parentDiv.children[i].id.toLowerCase());
 
+        //check if filter already exists, if yes then delete that filter. and set wantToRemove to true.
         if (item.innerHTML === parentDiv.children[i].innerText) {
-            deleteFilter(parentDiv.children[i].id);
+            if(parentDiv.children[i].id.includes("interest"))
+            {
+                amountOfInterestFilters--;
+            }
+            await deleteFilter(parentDiv.children[i].id);
             wantToRemove = true;
             break;
         }
-        if (result) {
+
+        //if standard filter is selected but not the same option, change filter text.
+        if (exists && classname !== "interest") {
             parentDiv.children[i].children[0].innerHTML = item.innerHTML;
             break;
 
         }
 
     }
-    if (result === false || parentDiv.children.length === 0 && wantToRemove === false) {
+    //todo, remove first added interest filter
+    if(amountOfInterestFilters === 5)
+    {
+        const parentDiv = document.getElementsByClassName('toegepaste-filters')[0];
+        console.log(" je hebt nu 5 filters");
+    }
+    //if filter doesnt exist or the parent has no filters, create filter.
+    if (exists === false || parentDiv.children.length === 0 ) {
 
+
+        if(wantToRemove === true)
+        {
+            return;
+        }
         switch (item.className) {
             case "optionGender":
 
@@ -63,12 +103,21 @@ function changeFilter(item) {
                 createFilter("age", item.innerHTML);
 
                 break;
+            case "optionInterest":
+
+                if(amountOfInterestFilters === 5)
+                {
+                    console.log("Too many filters");
+                    return;
+                }
+                amountOfInterestFilters++;
+                createFilterInterests("interest", item.innerHTML);
+                break;
         }
     }
-
 }
 
-
+//create div, give classname and id and create child in that div.
 function createFilter(typeFilter, valueFilter) {
 
     var parentDiv = document.createElement("div");
@@ -76,22 +125,39 @@ function createFilter(typeFilter, valueFilter) {
     parentDiv.id = typeFilter;
     var childDiv = document.createElement("p");
     childDiv.className = "filterOption";
-    childDiv.id = "option" + typeFilter;
+
+    childDiv.innerHTML = valueFilter;
+    parentDiv.appendChild(childDiv);
+
+    //set filter as child under filters.
+
+    document.getElementsByClassName("toegepaste-filters")[0].appendChild(parentDiv);
+    console.log("added standard filter");
+}
+//create div, give classname and id and create child in that div.
+function createFilterInterests(typeFilter, valueFilter) {
+
+    var parentDiv = document.createElement("div");
+    parentDiv.className = "chose";
+    parentDiv.id = typeFilter +valueFilter;
+    var childDiv = document.createElement("p");
+    childDiv.className = "filterOption";
 
     childDiv.innerHTML = valueFilter;
     parentDiv.appendChild(childDiv);
 
 
     document.getElementsByClassName("toegepaste-filters")[0].appendChild(parentDiv);
-    console.log("added");
+    console.log("added interest filter");
 }
 
-function deleteFilter(itemToRemove) {
+//delete filter.
+async function deleteFilter(itemToRemove) {
 
     var parentDiv = document.getElementsByClassName("toegepaste-filters")[0];
     var childToRemove = document.getElementById(itemToRemove);
     parentDiv.removeChild(childToRemove);
 
-    console.log("removed");
+    console.log("removed filter");
 
 }
