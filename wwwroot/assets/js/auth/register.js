c08662cb6d938050bfb203566218629893892722
Thijz <thijs.sonderman@hva.nl>
2021-11-25 14:41:17 +0100

    Registeren werkend (nog geen sessie wordt gestart)

--- wwwroot/assets/js/auth/register.js
+++ wwwroot/assets/js/auth/register.js
@@ -1,33 +1,22 @@
+document.querySelector("#submit-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+    register();
+})
 
 async function register() {
 
     //registeerdatum aanmaken
     let registerDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    console.log(registerDate)
 
     //aanmaken variabele voor het de inlog
-    let password = document.querySelector("#password").value;
-    let passwordVerify = document.querySelector("#passwordVerify").value;
-    let email = document.querySelector("#email").value;
-    let name = document.querySelector("#name").value;
-    let usertype = 2;
+    const password = document.querySelector("#password").value;
+    const passwordVerify = document.querySelector("#passwordVerify").value;
+    const email = document.querySelector("#email").value;
+    const name = document.querySelector("#name").value;
+    const usertype = 2; //gaat weg
 
-    //lees de naam, email en wachtwoord af uit de inputvelden
-    // document.querySelector("#password").addEventListener("change", event => {
-    //     password =  event.target.value;
-    // });
-    // document.querySelector("#passwordVerify").addEventListener("change", event => {
-    //     passwordVerify =  event.target.value;
-    // });
-    // document.querySelector("#email").addEventListener("change", event => {
-    //     email =  event.target.value;
-    // });
-
-
-    // document.querySelector("#name").addEventListener("change", event => {
-    //     console.log(event)
-    //     name =  event.target.value;
-    // });
+    // temp dummy data to try inserts
+    const username = "testusername" + Math.random(2000);
 
     //aanmaken profielfoto.
     let userPhotoId = name.replace(/\s+/g, '+').toLowerCase();
@@ -35,19 +24,27 @@ async function register() {
     // let profilePhoto = "https://ui-avatars.com/api/?name="+ userPhotoId +"?background=random";
 
     //laat de gebruiker alleen aanmelden als het wachtwoord het zelfde is.
+    console.log(username, email, password, registerDate, profilePhoto, usertype)
     if (password === passwordVerify) {
-        // const response = await FYSCloud.API.queryDatabase(
-        //     "INSERT INTO account (`email`, `password`, 'createdAt', 'profilePhoto', 'usertypeFk')" +
-        //     ` VALUES (${email}, ${password}, ${registerDate}, ${profilePhoto}, ${usertype})`
-        //  )
+        try {
+            //verstuur data naar database
+            await FYSCloud.API.queryDatabase("INSERT INTO account (username, email, password, createdAt," +
+                " profilePhoto, usertypeFk) VALUES (?, ?, ?, ?, ?, ?)",[username, email, password, registerDate,
+                profilePhoto, usertype]);
 
-        const response = await FYSCloud.API.queryDatabase("INSERT INTO account (email, password, createdAt, profilePhoto, usertypeFk) VALUES (email, password, registerDate, profilePhoto, userType)");
+            //hier komt sessie functie
 
-        await console.log(response)
+            //doorlinken naar profile page (als er geen error is).
+            window.location.replace("../../views/profile-edit.html");
+        }
+        catch {
+            //laat de gebruiker weten als er iets niet goed ging, of de email al is gebruikt
+            window.alert("Er is iets mis gegaan. :( \n Het kan zijn dat dit email address al in gebruik is.")
+        }
     }
     else {
+        //laat de gebruiker weten dat het wachtwoord niet klopt.
         document.querySelector("#error-message").innerHTML = "Je wachtwoord is niet het zelfde!";
     }
-
 
 }
