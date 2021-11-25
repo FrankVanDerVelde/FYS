8c656ab54c70be36a13c423283f20822e44e66d8
Thijz <thijs.sonderman@hva.nl>
2021-11-25 15:11:19 +0100

    Registeren werkend met de juiste invirment en een SHA2 HASH (nog geen sessie wordt gestart)

--- wwwroot/assets/js/auth/register.js
+++ wwwroot/assets/js/auth/register.js
@@ -13,7 +13,7 @@ async function register() {
     const passwordVerify = document.querySelector("#passwordVerify").value;
     const email = document.querySelector("#email").value;
     const name = document.querySelector("#name").value;
-    const usertype = 2; //gaat weg
+    const usertype = 1; //gaat weg
 
     // temp dummy data to try inserts
     const username = "testusername" + Math.random(2000);
@@ -24,23 +24,22 @@ async function register() {
     // let profilePhoto = "https://ui-avatars.com/api/?name="+ userPhotoId +"?background=random";
 
     //laat de gebruiker alleen aanmelden als het wachtwoord het zelfde is.
-    console.log(username, email, password, registerDate, profilePhoto, usertype)
     if (password === passwordVerify) {
-        try {
+        //try {
             //verstuur data naar database
-            await FYSCloud.API.queryDatabase("INSERT INTO account (username, email, password, createdAt," +
-                " profilePhoto, usertypeFk) VALUES (?, ?, ?, ?, ?, ?)",[username, email, password, registerDate,
-                profilePhoto, usertype]);
-
+            let log = await FYSCloud.API.queryDatabase("INSERT INTO account (email, password, username, profilePhoto," +
+                " usertypeFk, createdAt) VALUES (?, SHA2(?, 256), ?, ?, ?, ?)",[email, password, username,
+                profilePhoto, usertype, registerDate]);
+            console.log(log);
             //hier komt sessie functie
 
             //doorlinken naar profile page (als er geen error is).
-            window.location.replace("../../views/profile-edit.html");
-        }
-        catch {
+            //window.location.replace("../../views/profile-edit.html");
+        //}
+        //catch {
             //laat de gebruiker weten als er iets niet goed ging, of de email al is gebruikt
-            window.alert("Er is iets mis gegaan. :( \n Het kan zijn dat dit email address al in gebruik is.")
-        }
+            //window.alert("Er is iets mis gegaan. :( \n Het kan zijn dat dit email address al in gebruik is.")
+        //}
     }
     else {
         //laat de gebruiker weten dat het wachtwoord niet klopt.
--- wwwroot/assets/js/config.js
+++ wwwroot/assets/js/config.js
@@ -1,8 +1,6 @@
-import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
-
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is109_4.pwcUBjDBRQL238RC",
-    database: "fys_is109_4",
+    database: "fys_is109_4_dev",
     environment: "dev"
 });
