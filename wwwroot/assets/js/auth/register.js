b2ea66c08b3f299ffa44b9dd094b2dda298186dd
Thijz <thijs.sonderman@hva.nl>
2021-11-25 12:49:55 +0100

    alleen nog SQL query fixen

--- wwwroot/assets/js/auth/register.js
+++ wwwroot/assets/js/auth/register.js
@@ -1,30 +1,33 @@
-import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
 
- async function register() {
+async function register() {
 
     //registeerdatum aanmaken
-    let registerDate = Date.now();
+    let registerDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    console.log(registerDate)
 
     //aanmaken variabele voor het de inlog
-    let password;
-    let passwordVerify;
-    let email;
-    let name;
+    let password = document.querySelector("#password").value;
+    let passwordVerify = document.querySelector("#passwordVerify").value;
+    let email = document.querySelector("#email").value;
+    let name = document.querySelector("#name").value;
     let usertype = 2;
 
     //lees de naam, email en wachtwoord af uit de inputvelden
-    document.querySelector("#password").addEventListener("change", event => {
-        password =  event.target.value;
-    });
-    document.querySelector("#passwordVerify").addEventListener("change", event => {
-        passwordVerify =  event.target.value;
-    });
-    document.querySelector("#email").addEventListener("change", event => {
-        email =  event.target.value;
-    });
-    document.querySelector("#name").addEventListener("change", event => {
-        name =  event.target.value;
-    });
+    // document.querySelector("#password").addEventListener("change", event => {
+    //     password =  event.target.value;
+    // });
+    // document.querySelector("#passwordVerify").addEventListener("change", event => {
+    //     passwordVerify =  event.target.value;
+    // });
+    // document.querySelector("#email").addEventListener("change", event => {
+    //     email =  event.target.value;
+    // });
+
+
+    // document.querySelector("#name").addEventListener("change", event => {
+    //     console.log(event)
+    //     name =  event.target.value;
+    // });
 
     //aanmaken profielfoto.
     let userPhotoId = name.replace(/\s+/g, '+').toLowerCase();
@@ -33,14 +36,17 @@ import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
 
     //laat de gebruiker alleen aanmelden als het wachtwoord het zelfde is.
     if (password === passwordVerify) {
-        const response = await FYSCloud.API.queryDatabase(
-            "INSERT INTO `account` (`email`,`password`, 'createdAt', 'profilePhoto', 'usertypeFk')" +
-            `VALUES (${email} , ${password}, ${registerDate}, ${profilePhoto}, ${usertype})`
-         )
+        // const response = await FYSCloud.API.queryDatabase(
+        //     "INSERT INTO account (`email`, `password`, 'createdAt', 'profilePhoto', 'usertypeFk')" +
+        //     ` VALUES (${email}, ${password}, ${registerDate}, ${profilePhoto}, ${usertype})`
+        //  )
+
+        const response = await FYSCloud.API.queryDatabase("INSERT INTO account (email, password, createdAt, profilePhoto, usertypeFk) VALUES (email, password, registerDate, profilePhoto, userType)");
+
         await console.log(response)
     }
     else {
-        document.querySelector(".error-message").innerHTML = "Je wachtwoord is niet het zelfde!";
+        document.querySelector("#error-message").innerHTML = "Je wachtwoord is niet het zelfde!";
     }
 
 
