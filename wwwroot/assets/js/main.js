9fb8f2c983ab6ce0978ffe2d39f2bf74667450d4
Harmohat <harmohat.khangura@hva.nl>
2021-11-25 15:04:26 +0100

    added chat load dynamically

---
+++ wwwroot/assets/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    // if (FYSCloud.Session.get("loggedin")) {
+    //Add the chat modals to the page
+    const chat = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat.html");
+    addChat(chat);
+
+    function addChat(data) {
+        const firstElement = data[0];
+
+        document.querySelector("main").appendChild(firstElement);
+    }
+    // }
+});
