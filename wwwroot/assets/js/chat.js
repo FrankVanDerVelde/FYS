d568e6b301ccbaa37fb803a1abc6f9136063ebd6
Harmohat Khangura <harmohat.khangura@hva.nl>
2021-11-29 18:40:21 +0100

    Added chat visibility based on session check

--- wwwroot/assets/js/chat.js
+++ wwwroot/assets/js/chat.js
@@ -1,50 +1,52 @@
 window.onload = function(){
-    const showChatBtn = document.querySelector("#chat-btn");
-    const detailChatBox = document.querySelector("#detail-chat-box");
-    const chatBox = document.querySelector("#main-chat-box");
-    const closeChatBtn = document.querySelector(".close-chat");
-    const closeDetailChatBtn = document.querySelector(".close-detail-chat");
-    const goBackBtn = document.querySelector(".back-btn-chat");
+    if (FYSCloud.Session.get("loggedin")) {
+        const showChatBtn = document.querySelector("#chat-btn");
+        const detailChatBox = document.querySelector("#detail-chat-box");
+        const chatBox = document.querySelector("#main-chat-box");
+        const closeChatBtn = document.querySelector(".close-chat");
+        const closeDetailChatBtn = document.querySelector(".close-detail-chat");
+        const goBackBtn = document.querySelector(".back-btn-chat");
 
 
-    // show chat modal on click button
-    showChatBtn.addEventListener("click", () => {
-        chatBox.classList.remove("hide-chat");
-        chatBox.classList.add("show-chat");
-    });
+        // show chat modal on click button
+        showChatBtn.addEventListener("click", () => {
+            chatBox.classList.remove("hide-chat");
+            chatBox.classList.add("show-chat");
+        });
 
 
-    // go from detail chat modal to overview contacts messages
-    goBackBtn.addEventListener("click", () => {
-        detailChatBox.classList.remove("show-chat");
-        chatBox.classList.remove("hide-chat");
-        chatBox.classList.add("show-chat");
-    });
+        // go from detail chat modal to overview contacts messages
+        goBackBtn.addEventListener("click", () => {
+            detailChatBox.classList.remove("show-chat");
+            chatBox.classList.remove("hide-chat");
+            chatBox.classList.add("show-chat");
+        });
 
-    closeChatBtn.addEventListener("click", () => {
-        chatBox.classList.remove("show-chat");
-        chatBox.classList.add("hide-chat");
-    });
+        closeChatBtn.addEventListener("click", () => {
+            chatBox.classList.remove("show-chat");
+            chatBox.classList.add("hide-chat");
+        });
 
-    closeDetailChatBtn.addEventListener("click", () => {
-        detailChatBox.classList.remove("show-chat");
-        detailChatBox.classList.add("hide-chat");
-    });
+        closeDetailChatBtn.addEventListener("click", () => {
+            detailChatBox.classList.remove("show-chat");
+            detailChatBox.classList.add("hide-chat");
+        });
 
 
-    // check device, if phone make modal width 100%
-    const checkdevice =  () => {
-        const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-        if (isMobile) {
-            chatBox.style.width = "100%";
-            detailChatBox.style.width = "100%";
-        } else {
-            chatBox.style.width = "360px";
-            detailChatBox.style.width = "360px";
+        // check device, if phone make modal width 100%
+        const checkdevice =  () => {
+            const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+            if (isMobile) {
+                chatBox.style.width = "100%";
+                detailChatBox.style.width = "100%";
+            } else {
+                chatBox.style.width = "360px";
+                detailChatBox.style.width = "360px";
+            }
         }
-    }
 
-    checkdevice();
-    // on resize add checkdevice function
-    window.addEventListener("resize", checkdevice);
+        checkdevice();
+        // on resize add checkdevice function
+        window.addEventListener("resize", checkdevice);
+    }
 };
