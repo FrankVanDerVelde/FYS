99ea682309ff4d67b41cde781ae299f8dd7d27e9
Harmohat Khangura <harmohat.khangura@hva.nl>
2021-11-29 21:19:43 +0100

    Edit chat button visibility

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", async function () {
     if (FYSCloud.Session.get("loggedin")) {
-        //Add the chat modals to the page if session is set
+        // const button = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat-button.html");
+        // addChatButton(button);
+
+        //Add the chat modals to the page
         const chat = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat.html");
         addChat(chat);
 
+        // function addChatButton(data) {
+        //     const firstElement = data[0];
+        //     document.querySelector("main").insertBefore(firstElement, document.querySelector("main").firstChild);
+        // }
+
         function addChat(data) {
             const firstElement = data[0];
-
             document.querySelector("main").appendChild(firstElement);
         }
 
-        //Adds the chat visibilty toggle button to the page if session is set
-        const button = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat-button.html");
-        addChatButton(button);
+        document.querySelector("#chat-btn").style.visibility = "visible";
 
-        function addChatButton(data) {
-            const firstElement = data[0];
-
-            document.querySelector("main").appendChild(firstElement);
-        }
+     
     }
 });
+
