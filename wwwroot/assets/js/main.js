30bd458d2dd64c7d758de23318d5f3528227e365
Harmohat Khangura <harmohat.khangura@hva.nl>
2021-11-29 18:42:36 +0100

    Added chat modal toggle button

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    // if (FYSCloud.Session.get("loggedin")) {
-    //Add the chat modals to the page
-    const chat = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat.html");
-    addChat(chat);
+    if (FYSCloud.Session.get("loggedin")) {
+        //Add the chat modals to the page if session is set
+        const chat = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat.html");
+        addChat(chat);
 
-    function addChat(data) {
-        const firstElement = data[0];
+        function addChat(data) {
+            const firstElement = data[0];
 
-        document.querySelector("main").appendChild(firstElement);
+            document.querySelector("main").appendChild(firstElement);
+        }
+
+        //Adds the chat visibilty toggle button to the page if session is set
+        const button = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat-button.html");
+        addChatButton(button);
+
+        function addChatButton(data) {
+            const firstElement = data[0];
+
+            document.querySelector("main").appendChild(firstElement);
+        }
     }
-    // }
 });
