12d524a8a4823dc459bf56156b3e2ede3cee6bc6
Dinny van Huizen <dinnyvanhuizen@gmail.com>
2021-12-03 16:54:47 +0100

    worked on query

--- wwwroot/assets/js/matches-filter.js
+++ wwwroot/assets/js/matches-filter.js
@@ -4,26 +4,37 @@
 // interests sports, dansen, uitgaan, drinken, eten     max 5
 // "SELECT * FROM fys_is109_4_harmohat_chattest.account WHERE id = 1;"
 
-let foundUsers = [];
+//let foundUsers = [];
 // selectElement.addEventListener('change', (event) => {
 //     getResult();
 //   });
 
 
 async function getResult() {
-    let queryStrings = ["SELECT * FROM fys_is109_4_harmohat_chattest.account"];
+    let queryStrings = ["SELECT * FROM fys_is109_4_harmohat_chattest.account WHERE"];
     // Place get element on id's to get the values;
     let agesFilter = "1-80";
     let genderFilter = "man";
     let interestFilter = ["Interest1", "Interest2", "Interest3", "Interest4", "Interest5"];
 
     if (agesFilter) {
-        let ages = agesFilter.split('-');
+        let ages =[];
+        if (agesFilter.includes('+')) {
+            ages[0] = agesFilter.substring(0, 2);
+        }
+        else
+        {
+            ages = agesFilter.split('-');
+
+        }
         console.log(ages);
         //  StartTime BETWEEN DATEADD(HOUR, -1, GetDate())
         //         AND DATEADD(HOUR, 1, GetDate())
         if (ages.length === 1) {
-
+            let minDate = new Date();
+            minDate.setFullYear(minDate.getFullYear() - ages[0]);
+            minDate = minDate.toISOString().split("T")[0];
+            queryStrings.push(`WHERE birthdate > '${minDate}'`);
         } else {
             let birthdateInput = "10-15-1978"
 
@@ -34,18 +45,37 @@ async function getResult() {
             let dateCeiling = new Date();
             dateCeiling.setFullYear(dateCeiling.getFullYear() - ages[0]);
             dateCeiling = dateCeiling.toISOString().split("T")[0];
+            checkAnd(queryStrings);
 
-            queryStrings.push(`WHERE birthdate BETWEEN '${dateFloor}' AND '${dateCeiling}'`);
+            queryStrings.push(`birthdate BETWEEN '${dateFloor}' AND '${dateCeiling}'`);
         }
     }        
     
     if (genderFilter){
-        queryStrings.push(`AND gender = '${genderFilter}'`);
+        var genderNumber = 1;
+        switch (genderFilter)
+        {
+            case "man":
+                genderNumber = 1;
+                break;
+            case "vrouw":
+                genderNumber = 2;
+
+                break;
+            case "overig":
+                genderNumber = 3;
+
+                break;
+        }
+        checkAnd(queryStrings);
+        queryStrings.push(`gender = '${genderNumber}'`);
     }
-    
+
+
     if (interestFilter.length != 0){
         interestFilter.forEach(interest => {
-            queryStrings.push(`AND interest = '${interest}'`)
+            checkAnd(queryStrings);
+            queryStrings.push(`interest = '${interest}'`)
         });
     }
 
@@ -54,5 +84,15 @@ async function getResult() {
     await console.log(await FYSCloud.API.queryDatabase(queryStrings.join(' ')));
     
 }
+function checkAnd(queryStrings)
+{
+    console.log(queryStrings[0].includes("WHERE"));
 
+    if(!queryStrings[queryStrings.length-1].includes("WHERE"))
+    {
+        queryStrings.push(`AND`);
+        console.log("pushed and");
+
+    }
+}
 getResult()
