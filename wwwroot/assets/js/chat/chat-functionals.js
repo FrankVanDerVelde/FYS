1f9e6f1c5124a0994bc039c163007683c5a4715b
Harmohat Khangura <harmohat.khangura@hva.nl>
2021-12-05 23:27:06 +0100

    Added accept and decline buttons for match request

--- wwwroot/assets/js/chat/chat-functionals.js
+++ wwwroot/assets/js/chat/chat-functionals.js
@@ -1,26 +1,74 @@
+import * as module from './profanity-filter.js';
+
 window.addEventListener('load', async function () {
     if (FYSCloud.Session.get('loggedin')) {
+        const userSession = FYSCloud.Session.get('loggedin');
         const detailChatBox = document.querySelector('#detail-chat-box');
         const chatBox = document.querySelector('#main-chat-box');
-        const userSession = FYSCloud.Session.get('loggedin');
-
-        //Getting the whole [#chat-template] html piece
         const template = document.querySelector('#chat-template').innerHTML;
-        const userChatContact = await getHistoryList();
+        const requestsMainTemplate = document.querySelector('#requests-template').innerHTML;
+
+        //recieves the matches
+        const userChatContact = await getUserMatches(userSession[0].id);
+
+        const userRequests = await getPendingRequests(userSession[0].id);
+
+        for (let i = 0; i < userRequests.length; i++) {
+            const requestsTemplate = FYSCloud.Utils.parseHtml(requestsMainTemplate)[0];
+            const chatRequests = userRequests[i];
+
+            requestsTemplate.dataset.id = chatRequests.id;
+            requestsTemplate.querySelector('.accept-request-btn').dataset.id = chatRequests.id;
+            requestsTemplate.querySelector('.decline-request-btn').dataset.id = chatRequests.id;
+
+            requestsTemplate.querySelector('.request-profile-pic').src = chatRequests.profilePhoto;
+            requestsTemplate.querySelector('.request-title').innerHTML = chatRequests.name;
+
+            document.querySelector('.requests-list').appendChild(requestsTemplate);
+        }
 
         for (let i = 0; i < userChatContact.length; i++) {
+            //Parsing the html template element for the contacts list
             const chatTemplate = FYSCloud.Utils.parseHtml(template)[0];
             const chatContacts = userChatContact[i];
-
+            
+            //Sets the id from chatContacts for the data-id of chatTemplate 
             chatTemplate.dataset.id = chatContacts.id;
 
-            //Assigning the data from DB to the html innerHTML
+            //Assigning the data from DB to the template attributes
             chatTemplate.querySelector('.message-profile-pic').src = chatContacts.profilePhoto;
-            chatTemplate.querySelector('.chat-title').innerHTML = `${chatContacts.firstname} ${chatContacts.lastname}`;
+            chatTemplate.querySelector('.chat-title').innerHTML = chatContacts.name;
             
+            chatTemplate.querySelector('.contact-option').dataset.id = chatContacts.id;
+            chatTemplate.querySelector('#chat-dropdown-options').dataset.id = chatContacts.id;
+            chatTemplate.querySelector('.block-btn').dataset.id = chatContacts.id;
+
             //Appending child [chatTemplate] to [.chat-list]
             document.querySelector('.chat-list').appendChild(chatTemplate);
-            
+        
+            // Looping through all the `contact options`
+            document.querySelectorAll('.contact-option').forEach(function(parentDiv) {
+                //Adding a click listenere to the parentDiv
+                parentDiv.addEventListener('click', (event) => {
+                    //Prevents that the `li` element also will be clicked
+                    event.stopPropagation();
+                    // Looping through all the `.contact-dropdown-content`
+                    document.querySelectorAll('.contact-dropdown-content').forEach(function(childDiv) {
+                        // If the date-id of parentDiv equals the data-id of childDiv, the display for childDiv will be set to `block`
+                        if(parentDiv.dataset.id == childDiv.dataset.id){
+                            childDiv.style.display = 'block'; 
+                            document.querySelectorAll('.block-btn').forEach(function(button) {
+                                button.addEventListener('click', () => {
+                                    //Block user function
+                                });         
+                            });
+                        }else{
+                            childDiv.style.display = 'none';    
+                        }
+                    });
+                });
+            });
+                
             //When user clicks on a chat from the list it will show the detailed message box
             chatTemplate.addEventListener('click', async function () {
                 const recieverId = this.dataset.id;
@@ -30,6 +78,16 @@ window.addEventListener('load', async function () {
                 const array = [currentUser, recipientUser];
                 const arrayDate = [];
                 let reciever = [];
+
+                //Removes the `li` elements when new chat is opened
+                function removeChats(parent) {
+                    while (parent.firstChild) {
+                        parent.removeChild(parent.firstChild);
+                    }
+                }
+                
+                const messagesList = document.querySelector('.messages-list');
+                removeChats(messagesList);
                 
                 //Set recieverId value to hidden input [insert bug fix]
                 document.querySelector('.hidden-input-id').value = recieverId;
@@ -40,6 +98,7 @@ window.addEventListener('load', async function () {
                     }
                 });
             
+                //Sorting the recieved chat data on date
                 const arrayChats = arrayDate.sort(function(a,b){
                     if (a.createdAt > b.createdAt) return 1;
                     if (a.createdAt < b.createdAt) return -1;
@@ -51,7 +110,7 @@ window.addEventListener('load', async function () {
                 detailChatBox.classList.remove('hide-chat');
                 chatBox.classList.add('hide-chat');
                 chatBox.classList.remove('show-chat');
-                
+      
                 for (let i = 0; i < recieverData.length; i++) {
                     reciever = recieverData[i];
                 }
@@ -65,14 +124,18 @@ window.addEventListener('load', async function () {
                     }
                 });
                 
-                //adding name and profile photo of the recipient in the detail message box
-                document.querySelector('.detail-chat-header').innerHTML = reciever.firstname;
+                //adding name and profile photo of the reciever in the detail message box
+                document.querySelector('.detail-chat-header').innerHTML = reciever.name;
                 document.querySelector('.detail-chat-photo').src = reciever.profilePhoto;
                 
                 for (let i = 0; i < arrayChats.length; i++) {
                     const message = arrayChats[i];
                     let msgExists = false;
 
+                    // Converting the message time to `toLocaleString`
+                    const messageFullDatetime = new Date(message.createdAt);
+                    const messageConvertedTime = messageFullDatetime.toLocaleString();
+                    
                     //checks on duplicate data in detailed chat
                     document.querySelectorAll('li').forEach(function(element) {
                         if (element.getAttribute('data-id') != null) {
@@ -82,56 +145,85 @@ window.addEventListener('load', async function () {
                         }
                     });
 
+                    // If the message does not exists the program will add th chats as `li` elements based on current user and reciever
                     if (!msgExists) {
                         let msg = (message.recieverFk == userSession[0].id) ?
-                            `<li class="left-chat" data-id="msg-${message.id}" data-sender="${userSession[0].id}""><p><b>${message.username}</b> :  ${message.message}</p></li>`:
-                            `<li class="right-chat" data-id="msg-${message.id}" data-reciever="${recieverId}"><p><b>${message.username}</b> :  ${message.message}</p></li>`;
+                            `<li class="left-chat" data-id="msg-${message.id}" data-sender="${userSession[0].id}"><p><b>${message.name}</b> :  
+                            ${message.message} <p class="messages-time">${messageConvertedTime}</p></p></li>`:
 
+                            `<li class="right-chat" data-id="msg-${message.id}" data-reciever="${recieverId}"><p><b>${message.name}</b> :  
+                            ${message.message} <p class="messages-time">${messageConvertedTime}</p></p></li>`;
+                        
+                        // Adds the message to `.messages-list` (`ul` element)
                         document.querySelector('.messages-list').innerHTML += msg;
                     }
                 }
 
-                document.querySelectorAll('.left-chat').forEach(function(element) {
-                    if (element.getAttribute('data-sender') != null) {
-                        if (element.getAttribute('data-sender') !=  userSession[0].id) {
-                            element.style.display = 'none';
-                        }else{
-                            element.style.display = 'inline-block';
-                        }
-                    }
-                });
-
-                document.querySelectorAll('.right-chat').forEach(function(element) {
-                    if (element.getAttribute('data-reciever') != null) {
-                        if (element.getAttribute('data-reciever') !=  recieverId) {
-                            element.style.display = 'none';
-                        }else{
-                            element.style.display = 'block';
-                        }
-                    }
-                });
-
                 document.querySelector('.chat-form').addEventListener('submit', async function(e){
+                    //preventing the refresh action from the form
                     e.preventDefault();
                     let message = document.forms['chat-form']['msg-input'].value;
                     let userId = document.querySelector('.hidden-input-id').value;
+                    //Checking if the message is nt empty
                     if (message != '') {
-                        insertMessage(userSession[0].id, userId, message);
+                        // Using the function censorProfanity in `profanity-filter.js` to censor words before the insert
+                        message = module.censorProfanity(message);
+                        await insertMessage(userSession[0].id, userId, message);
                         document.querySelector('.chat-form').reset();
                     }
                 });
             });
         }
 
+
+        document.querySelectorAll('.accept-request-btn').forEach(function(button) {
+            button.addEventListener('click', async function (){
+                console.log(button.dataset.id);
+                const update = await updatePendingRequests('accepted', userSession[0].id, button.dataset.id); 
+                console.log(update);
+            });
+        });
+
+        document.querySelectorAll('.decline-request-btn').forEach(function(button) {
+            button.addEventListener('click', () => {
+                await updatePendingRequests(userSession[0].id, button.dataset.id, 'declined');
+            });
+        });
+
         /**
-         *
+         * @param {int} userId 
+         * @returns the matches from table `matches` which are pending
+         */
+        async function updatePendingRequests(status, userId, matchedUserId){
+            try {
+                const query = FYSCloud.API.queryDatabase(`UPDATE matches SET status = ? WHERE currUserFk = ?  AND matchedUserFk = ?`, [status, userId, matchedUserId]);
+                const results = await query;
+                return await results;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        async function getPendingRequests(userId){
+            try {
+                const query = FYSCloud.API.queryDatabase(`SELECT * FROM matches INNER JOIN account ON matchedUserFk = account.id WHERE currUserFk = ? AND status = 'pending'`, [userId]);
+                const results = await query;
+                return await results;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    
+        /**
+         * Function selects the user messages + account data where senderFk equals senderId and recieverFk equals recieverId
+         * 
          * @param {int} senderId - id from the user who sent the message (current user)
          * @param {int} recieverId - id from the user who will reciever the message from current user (recipient)
          * @returns JSON with user message data from the database
          */
         async function getUserMessages(senderId, recieverId){
             try {
-                const query = FYSCloud.API.queryDatabase('SELECT messages.id, account.username, messages.message, messages.recieverFk, messages.createdAt FROM messages INNER JOIN account ON senderFk = account.id WHERE senderFk = ? AND recieverFk = ? ORDER BY messages.createdAt;', [senderId, recieverId]);
+                const query = FYSCloud.API.queryDatabase(`SELECT messages.id, account.name, messages.message, messages.recieverFk, messages.createdAt FROM messages INNER JOIN account ON senderFk = account.id WHERE senderFk = ? AND recieverFk = ? ORDER BY messages.createdAt;`, [senderId, recieverId]);
                 const results = await query;
                 return await results;
             } catch (error) {
@@ -139,14 +231,16 @@ window.addEventListener('load', async function () {
             }
         }
 
+
         /**
-         *
+         *!TODO: Get users from matches
+
          * @param {int} userId
          * @returns JSON with messages which interacted with the current user
          */
-        async function getHistoryList(){
+        async function getUserMatches(userId){
             try {
-                const query = FYSCloud.API.queryDatabase('SELECT * FROM account INNER JOIN person ON person.accountFk = account.id');
+                const query = FYSCloud.API.queryDatabase('SELECT * FROM matches INNER JOIN account ON matchedUserFk = account.id WHERE currUserFk = ? AND status = "accepted"', [userId]);
                 const results = await query;
                 return await results;
             } catch (error) {
@@ -155,7 +249,8 @@ window.addEventListener('load', async function () {
         }
 
         /**
-         *
+         * Inserts the messages into table `messages`
+         * 
          * @param {int} senderId
          * @param {int} recieverId
          * @param {string} message
@@ -172,13 +267,14 @@ window.addEventListener('load', async function () {
         }
 
         /**
-         *
+         * Function selects the data from table `account` where id equals the id from the chat reciever
+         * 
          * @param {int} recieverId
          * @returns the data from message reciever
          */
         async function getReciever(recieverId){
             try {
-                const query = FYSCloud.API.queryDatabase('SELECT * FROM person INNER JOIN account ON person.accountFk = account.id WHERE account.id = ?;', [recieverId]);
+                const query = FYSCloud.API.queryDatabase('SELECT * FROM account WHERE account.id = ?;', [recieverId]);
                 const results = await query;
                 return await results;
             } catch (error) {
