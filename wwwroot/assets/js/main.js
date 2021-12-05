ebc107b5ee15669c3d5ec9e168c98f1558d622b7
Harmohat Khangura <harmohat.khangura@hva.nl>
2021-12-05 23:28:43 +0100

    Fixed document loading error

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -1,17 +1,22 @@
-document.addEventListener("DOMContentLoaded", async function () {
-    if (FYSCloud.Session.get("loggedin")) {
-        //Add the chat modals to the page
-        const chat = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat.html");
-        addChat(chat);
+if (FYSCloud.Session.get("loggedin")) {
+    loadChat();
 
-        function addChat(data) {
-            const firstElement = data[0];
-            document.querySelector("main").appendChild(firstElement);
-        }
+}else{
+    document.querySelector("#chat-btn").style.visibility = "hidden";
+}
 
-        document.querySelector("#chat-btn").style.visibility = "visible";
-    }else{
-        document.querySelector("#chat-btn").style.visibility = "hidden";
+async function loadChat(){
+    //Add the chat modals to the page
+    const chat = await FYSCloud.Utils.fetchAndParseHtml("../views/components/_chat.html");
+    addChat(chat);
+
+    function addChat(data) {
+        const firstElement = data[0];
+        document.querySelector("main").appendChild(firstElement);
     }
-});
+
+    document.querySelector("#chat-btn").style.visibility = "visible";
+}
+
+
 
