8bf4470fd57b4cacc295f3ac2fd43efbfd37673a
VEENMAM3 <VEENMAM3@ad.hva.nl>
2021-12-07 15:51:31 +0100

    Mis een klein beetje style updates nodig, alle logica zit er in!

--- wwwroot/assets/js/admin/panel.js
+++ wwwroot/assets/js/admin/panel.js
@@ -1,1 +1,38 @@
+async function searchUser() {
+    const searchParam = document.getElementById("searchInput").value;
 
+    console.log(searchParam)
+
+    const data = [
+        await getUserByEmailAsync(searchParam),
+        await getUserByName(searchParam)
+    ];
+
+    for(let i=0; i<data.length; i++) {
+        if(data[i] != null) {
+            await updatePanel(data[i]);
+        }
+    }
+
+}
+
+async function updatePanel(userObj) {
+    document.getElementById("name").innerText = userObj.name;
+    document.getElementById("email").innerText = userObj.email;
+    document.getElementById("userImage").src = userObj.profilePhoto;
+
+    if(userObj.lastLoggedIn != null) {
+        document.getElementById("lastActive").innerText = userObj.lastLoggedIn;
+    }
+
+    // To be implemented
+    /*document.getElementById("matchAmount").innerText = await getMatchAmount(); */
+}
+
+async function deleteCurrentUser() {
+    const user = await getUserByEmailAsync(document.getElementById("email").innerText);
+
+    // console.log(userIntrests)
+
+    await deleteUserById(user.id);
+}
--- wwwroot/assets/js/admin/table.js
+++ wwwroot/assets/js/admin/table.js
@@ -1,49 +1,150 @@
-window.onload = async function() {
+window.onload = async function () {
     await getMaxPages();
+    await loadTable();
 }
 
-function decrementPage() {
+async function decrementPage() {
     let currentPage = document.getElementById("page").value;
     let newPage = currentPage - 1;
-    if(newPage <= 0)
+    if (newPage <= 0)
         newPage = 1;
 
     // getMaxPages()
     document.getElementById("page").value = newPage;
+    await loadTable();
 }
 
 async function incrementPage() {
     let currentPage = document.getElementById("page").value;
     let newPage = Number(currentPage) + 1;
-    if(newPage > await getMaxPages())
+    if (newPage > await getMaxPages())
         newPage = await getMaxPages()
 
     // getMaxPages()
     document.getElementById("page").value = newPage;
+    await loadTable();
 }
 
 async function getMaxPages() {
     let totalUsers = await getAllUsersAsync();
 
-    let totalPages = Math.round(totalUsers.length / 8);
+    let totalPages = Math.ceil(totalUsers.length / 8);
 
 
     document.getElementById("pages").innerHTML = "\\" + totalPages;
     return totalPages;
 }
 
-function openDeleteDialog() {
-    document.getElementById("deleteDialog").style.display='block';
+function getCurrentPage() {
+    return parseInt(document.getElementById("page").value);
 }
 
-function closeDelelteDialog() {
-    document.getElementById("deleteDialog").style.display='none';
+async function openDeleteDialog(userId) {
+    const user = await getUserByIdAsync(userId);
+    document.getElementById("deleteDialog").style.display = 'block';
+
+    document.getElementById("deleteUser").addEventListener("click", async function () {
+        const controlInput = document.getElementById("controlUsername").value;
+
+        if (controlInput === user.name) {
+            await deleteUserById(userId);
+            location.reload();
+        }
+    });
 }
 
-function openEditDialog() {
-    document.getElementById("editDialog").style.display='flex';
+function closeDeleteDialog() {
+    document.getElementById("deleteDialog").style.display = 'none';
+}
+
+async function updateUser(userId) {
+    const image = document.getElementById("userImage").src;
+    const name = document.getElementById("name").value;
+    const birthDate = document.getElementById("birthDate").value;
+    const bio = document.getElementById("bio").value;
+    const email = document.getElementById("email").value;
+
+    const columns = ['name', 'bio', 'birthDate', 'email'];
+    await sqlUpdateAsync("account", columns, [name, bio, birthDate, email], userId);
+}
+
+async function openEditDialog(userId) {
+    document.getElementById("editDialog").style.display = 'flex';
+    const user = await getUserByIdAsync(userId);
+
+    document.getElementById("userImage").src = user.profilePhoto;
+    document.getElementById("name").value = user.name;
+    document.getElementById("birthDate").value = await stripTimeFromDate(user.birthdate);
+    document.getElementById("bio").value = user.bio;
+    document.getElementById("email").value = user.email;
+
+    const selectBox = document.getElementById("userType");
+    if(user.usertypeFk === 1) {
+        selectBox.options[1].selected = true;
+    } else if (user.usertypeFk === 2){
+        selectBox.options[0].selected = true;
+    }
+
+    document.getElementById("save-button").addEventListener("click", async function() {
+        await updateUser(userId);
+        location.reload();
+    });
 }
 
 function closeEditDialog() {
-    document.getElementById("editDialog").style.display='none';
+    document.getElementById("editDialog").style.display = 'none';
+}
+
+async function loadTable() {
+    // Clear the table first for changes!
+    document.getElementById("tableArray").innerHTML = "";
+
+    let splittedUsers = await splitArray(await getAllUsersAsync());
+    let cellArray = [];
+
+    // minus one bc arrays start at 0!
+    let currentTab = getCurrentPage() - 1;
+
+    const header = `
+            <thead>
+                <tr>
+                    <th>Naam</th>
+                    <th>Email</th>
+                    <th>Woonplaats</th>
+                    <th>Opties</th>
+                </tr>
+            </thead>`.replace(/\n/g, '');
+
+    cellArray.push(header);
+
+    for (let i = 0; i < splittedUsers[currentTab].length; i++) {
+        const user = splittedUsers[currentTab][i];
+
+        let html = `<tr> 
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>${user.location}</td>
+                        <td>
+                            <button class="edit-button" onclick="openEditDialog(${user.id})">Edit</button>
+                            <button class="delete-button" onclick="openDeleteDialog(${user.id})">Delete</button>
+                        </td>
+                    </tr>`.replace(/\n/g, '');
+
+        cellArray.push(html);
+    }
+
+    for(let i=0; i<cellArray.length; i++) {
+        document.getElementById("tableArray").innerHTML += cellArray[i];
+    }
+
+}
+
+async function splitArray(array, amountPerArray = 8) {
+    let splittedArray = [];
+
+    while(array.length > 0) {
+        splittedArray.push(array.splice(0, amountPerArray));
+    }
+
+    return splittedArray;
 }
