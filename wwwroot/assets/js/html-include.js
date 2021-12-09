99cd7986394e12952015d23f5dd83d53a29d0c9b
FrankVanDerVelde <frankvdv1998@gmail.com>
2021-12-09 14:45:52 +0100

    Profile page tweaks and added code to start using partials

---
+++ wwwroot/assets/js/html-include.js
@@ -0,0 +1,25 @@
+const partials = [{
+    "element_name": "header",
+    "html_file_name": "header.html"
+
+},
+{
+    "element_name": "footer",
+    "html_file_name": "footer.html"
+
+}]
+partials.forEach(partialInfo => {
+    const {element_name:elementName, html_file_name:fileName} = partialInfo;
+
+    const element = document.querySelector(elementName);
+    const fileUrl = `partials/${fileName}`;
+
+    fetch(fileUrl).then(res => {
+        return res.text();
+    }).then(function(html) {
+        var parser = new DOMParser();
+	    var doc = parser.parseFromString(html, 'text/html');
+
+        element.parentNode.replaceChild(doc.querySelector(partialInfo.element_name), element);
+    });
+});
