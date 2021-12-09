6c52760508b8dba55c41e649d909c5897a728493
Dinny van Huizen <dinnyvanhuizen@gmail.com>
2021-12-10 00:21:47 +0100

    moved all script tags in html to head, updated nav bar for text matches, added css for profile foto, disabled ?matches.link?

--- wwwroot/assets/js/inlog-change.js
+++ wwwroot/assets/js/inlog-change.js
@@ -4,15 +4,22 @@ let session = FYSCloud.Session.get('loggedin');
 
     if (typeof(session) !== "undefined") {
         let profilePhoto = session[0].profilePhoto;
-        document.querySelector('.matches-link').style.display = 'block';
+      //  document.querySelector('.matches-link').style.display = 'block';
         document.querySelector('.logout-btn').style.display = 'block';
-        document.querySelector('.inlog-btn').style.display = 'none';
-        document.querySelector('.registreer-btn').style.display = 'none';
+        if(document.querySelector('.inlog-btn') !== null){
+            document.querySelector('.inlog-btn').style.display = 'none';
+        }
+        if(document.querySelector('.registreer-btn') !== null){
+            document.querySelector('.registreer-btn').style.display = 'none';
+        }
         document.querySelector('.profile-button').style.display = 'block';
-        document.querySelector('.footer-login').style.display = 'none';
+
+        if(document.querySelector('.footer-login') !== null){
+            document.querySelector('.footer-login').style.display = 'none';
+        }
         document.getElementById("header-profile-img").src = profilePhoto;
     } else {
-        document.querySelector('.matches-link').style.display = 'none';
+      //  document.querySelector('.matches-link').style.display = 'none';
         document.querySelector('.logout-btn').style.display = 'none';
         document.querySelector('.inlog-btn').style.display = 'block';
         document.querySelector('.registreer-btn').style.display = 'block';
