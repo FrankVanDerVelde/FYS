b37ee1875dabe91e02862429ccdde05bb85866b5
FrankVanDerVelde <frankvdv1998@gmail.com>
2021-12-09 16:44:39 +0100

    Included partials in most pages

--- wwwroot/assets/js/html-include.js
+++ wwwroot/assets/js/html-include.js
@@ -8,18 +8,20 @@ const partials = [{
     "html_file_name": "footer.html"
 
 }]
-partials.forEach(partialInfo => {
-    const {element_name:elementName, html_file_name:fileName} = partialInfo;
+document.addEventListener('DOMContentLoaded', async function () {
+    partials.forEach(partialInfo => {
+        const { element_name: elementName, html_file_name: fileName } = partialInfo;
 
-    const element = document.querySelector(elementName);
-    const fileUrl = `partials/${fileName}`;
+        const element = document.querySelector(elementName);
+        const fileUrl = `partials/${fileName}`;
 
-    fetch(fileUrl).then(res => {
-        return res.text();
-    }).then(function(html) {
-        var parser = new DOMParser();
-	    var doc = parser.parseFromString(html, 'text/html');
+        fetch(fileUrl).then(res => {
+            return res.text();
+        }).then(function (html) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, 'text/html');
 
-        element.parentNode.replaceChild(doc.querySelector(partialInfo.element_name), element);
+            element.parentNode.replaceChild(doc.querySelector(partialInfo.element_name), element);
+        });
     });
 });
