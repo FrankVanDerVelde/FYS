b8b13cd6fff98be180bc9b81d8d859999b87de4d
Lorenzo Matruglio <lorenzo.matruglio@hva.nl>
2021-12-09 12:55:12 +0100

    Gemaakt
    lorenzo gemaakte html changes js on loggedin

--- wwwroot/assets/js/inlog-change.js
+++ wwwroot/assets/js/inlog-change.js
@@ -9,11 +9,15 @@ let session = FYSCloud.Session.get('loggedin');
         document.querySelector('.logout-btn').style.display = 'block';
         document.querySelector('.inlog-btn').style.display = 'none';
         document.querySelector('.registreer-btn').style.display = 'none';
+        document.querySelector('.profile-button').style.display = 'block';
+
 
     } else {
         document.querySelector('.matches-link').style.display = 'none';
         document.querySelector('.logout-btn').style.display = 'none';
         document.querySelector('.inlog-btn').style.display = 'block';
         document.querySelector('.registreer-btn').style.display = 'block';
+        document.querySelector('.profile-button').style.display = 'none';
+
     }
 });
