094b85715f7ee1155dbb8497d8b227f8671f30fc
Frank <frankvdv1998@gmail.com>
2021-12-10 13:45:42 +0100

    Fixed navbar and improved it

--- wwwroot/assets/js/inlog-change.js
+++ wwwroot/assets/js/inlog-change.js
@@ -1,29 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 let session = FYSCloud.Session.get('loggedin');
+    const logoutButtons = document.getElementsByClassName('logout-btn');
+    const loginButtons = document.getElementsByClassName('login-btn');
+    const registerButtons = document.getElementsByClassName('register-btn');
+    const profileButtons = document.getElementsByClassName('profile-button');
+    const matchesLink = document.getElementsByClassName('matches-link');
+
+    function setVisbility (targets, visibility) {
+        console.log(targets);
+        for (i = 0; i < targets.length; i++) {
+            targets[i].style.display = visibility;
+        }
+    }
 
     if (typeof(session) !== "undefined") {
-        let profilePhoto = session[0].profilePhoto;
-      //  document.querySelector('.matches-link').style.display = 'block';
-        document.querySelector('.logout-btn').style.display = 'block';
-        if(document.querySelector('.inlog-btn') !== null){
-            document.querySelector('.inlog-btn').style.display = 'none';
-        }
-        if(document.querySelector('.registreer-btn') !== null){
-            document.querySelector('.registreer-btn').style.display = 'none';
-        }
-        document.querySelector('.profile-button').style.display = 'block';
+        // If profile photo exists use it. Otherwise generate a default
+        let profilePhoto = (session[0].profilePhoto ? session[0].profilePhoto : `https://ui-avatars.com/api/?name=${session[0].name}?background=#e0dcdc`);
 
-        if(document.querySelector('.footer-login') !== null){
-            document.querySelector('.footer-login').style.display = 'none';
-        }
+        // Setting display attribute to empty instead of re-setting it to prevent conflicts with classes
+        setVisbility(matchesLink, '');
+        setVisbility(logoutButtons, '');
+        setVisbility(loginButtons, 'none');
+        setVisbility(registerButtons, 'none');
+        setVisbility(profileButtons, '');
         document.getElementById("header-profile-img").src = profilePhoto;
     } else {
-      //  document.querySelector('.matches-link').style.display = 'none';
-        document.querySelector('.logout-btn').style.display = 'none';
-        document.querySelector('.inlog-btn').style.display = 'block';
-        document.querySelector('.registreer-btn').style.display = 'block';
-        document.querySelector('.profile-button').style.display = 'none';
-        document.querySelector('.footer-login').style.display = 'block';
+        setVisbility(logoutButtons, 'none');
+        setVisbility(loginButtons, '');
+        setVisbility(registerButtons, '');
+        setVisbility(profileButtons, 'none');
+        setVisbility(matchesLink, '');
     }
 });
