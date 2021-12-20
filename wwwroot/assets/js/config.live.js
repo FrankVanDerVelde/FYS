dbd5a3a94d9aded7a933b5f77a4ddadcd23a8d64
Frank <frankvdv1998@gmail.com>
2021-12-20 17:09:22 +0100

    Testing more stuff

--- wwwroot/assets/js/config.live.js
+++ wwwroot/assets/js/config.live.js
@@ -1,3 +1,5 @@
+console.log('Using live db');
+
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is109_4.pwcUBjDBRQL238RC",
--- wwwroot/assets/js/config.mockup.js
+++ wwwroot/assets/js/config.mockup.js
@@ -1,3 +1,5 @@
+console.log('Using mockup db');
+
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is109_4.pwcUBjDBRQL238RC",
