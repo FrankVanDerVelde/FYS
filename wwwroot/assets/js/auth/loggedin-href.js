8b0ecdc1381efb4bde238263deac5fb28488bd3c
Thijz <thijs.sonderman@hva.nl>
2021-12-23 12:31:45 +0100

    head update for register.html & loggedin-href.js update for admins

--- wwwroot/assets/js/auth/loggedin-href.js
+++ wwwroot/assets/js/auth/loggedin-href.js
@@ -2,7 +2,9 @@
 window.addEventListener("load", function() {
     let loggedin = FYSCloud.Session.get("loggedin");
 
-    if (loggedin) {
+    if (loggedin && loggedin[0].usertypeFk === 2) {
         window.location.replace("../../views/profile-edit.html");
+    } else if (loggedin && loggedin[0].usertypeFk === 1) {
+        window.location.replace("../../views/admin/panel.html");
     }
 });
