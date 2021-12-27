ae8babb3759af92f48fec2224037c24bae7b571d
Frank <frankvdv1998@gmail.com>
2021-12-27 20:15:52 +0100

    Fixed the errors caused by a faulty merge

--- wwwroot/assets/js/inlog-change.js
+++ wwwroot/assets/js/inlog-change.js
@@ -1,53 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-let session = FYSCloud.Session.get('loggedin');
-
-    if (typeof(session) !== "undefined") {
-        let profilePhoto = session[0].profilePhoto;
-      //  document.querySelector('.matches-link').style.display = 'block';
-        document.querySelector('.logout-btn').style.display = 'block';
-        if(document.querySelector('.inlog-btn') !== null){
-            document.querySelector('.inlog-btn').style.display = 'none';
+    let session = FYSCloud.Session.get('loggedin');
+        const logoutButtons = document.getElementsByClassName('logout-btn');
+        const loginButtons = document.getElementsByClassName('login-btn');
+        const registerButtons = document.getElementsByClassName('register-btn');
+        const profileButtons = document.getElementsByClassName('profile-button');
+        const matchesLink = document.getElementsByClassName('matches-link');
+    
+        function setVisbility (targets, visibility) {
+            for (i = 0; i < targets.length; i++) {
+                targets[i].style.display = visibility;
+            }
         }
-        if(document.querySelector('.registreer-btn') !== null){
-            document.querySelector('.registreer-btn').style.display = 'none';
+    
+        if (typeof(session) !== "undefined") {
+            // If profile photo exists use it. Otherwise generate a default
+            let profilePhoto = (session[0].profilePhoto ? session[0].profilePhoto : `https://ui-avatars.com/api/?name=${session[0].name}?background=#e0dcdc`);
+    
+            // Setting display attribute to empty instead of re-setting it to prevent conflicts with classes
+            setVisbility(matchesLink, '');
+            setVisbility(logoutButtons, '');
+            setVisbility(loginButtons, 'none');
+            setVisbility(registerButtons, 'none');
+            setVisbility(profileButtons, '');
+    
+            if (session[0].usertypeFk == 1) {
+                let userPanel = document.createElement('a');
+                let panel = document.createElement('a');
+    
+                let userPanelLink = document.createTextNode("User Panel"); 
+                let panelLink = document.createTextNode("Panel");
+
+                userPanel.className = 'admin-links';
+                panel.className = 'admin-links';
+
+                userPanel.appendChild(userPanelLink); 
+                panel.appendChild(panelLink); 
+                      
+                // Set the href property
+                userPanel.href ="/wwwroot/assets/views/admin/userPanel.html"; 
+                panel.href = "/wwwroot/assets/views/admin/panel.html"; 
+    
+                document.querySelector('#dropdown-content').prepend(userPanel, panel); 
+            } else {
+                document.querySelectorAll('.admin-link').forEach(link => link.remove());
+            }
+    
+            document.getElementById("header-profile-img").src = profilePhoto;
+        } else {
+            setVisbility(logoutButtons, 'none');
+            setVisbility(loginButtons, '');
+            setVisbility(registerButtons, '');
+            setVisbility(profileButtons, 'none');
+            setVisbility(matchesLink, '');
         }
-        document.querySelector('.profile-button').style.display = 'block';
-
-        if(document.querySelector('.footer-login') !== null){
-            document.querySelector('.footer-login').style.display = 'none';
-        }
-
-        if (session[0].usertypeFk == 1) {
-            console.log('admin');
-            let userPanel = document.createElement('a');
-            let panel = document.createElement('a');
-
-            let userPanelLink = document.createTextNode("User Panel"); 
-            let panelLink = document.createTextNode("Panel"); 
-
-            userPanel.className = 'admin-links';
-            panel.className = 'admin-links';
-
-            userPanel.appendChild(userPanelLink); 
-            panel.appendChild(panelLink); 
-                  
-            // Set the href property
-            userPanel.href ="/wwwroot/assets/views/admin/userPanel.html"; 
-            panel.href = "/wwwroot/assets/views/admin/panel.html"; 
-
-            document.querySelector('#dropdown-content').prepend(userPanel, panel); 
-        }else{
-            document.querySelectorAll('.admin-link').forEach(link => link.remove());
-        }
-        
-        document.getElementById("header-profile-img").src = profilePhoto;
-    } else {
-      //  document.querySelector('.matches-link').style.display = 'none';
-        document.querySelector('.logout-btn').style.display = 'none';
-        document.querySelector('.inlog-btn').style.display = 'block';
-        document.querySelector('.registreer-btn').style.display = 'block';
-        document.querySelector('.profile-button').style.display = 'none';
-        document.querySelector('.footer-login').style.display = 'block';
-    }
-});
+    });    
