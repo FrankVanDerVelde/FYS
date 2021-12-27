6a2e65d505644ca373dc1fbb7ba9d40e35cdba2f
Frank <frankvdv1998@gmail.com>
2021-12-27 21:42:59 +0100

    Another try to fix image uploading on dev

--- wwwroot/assets/js/profile/profile-edit.js
+++ wwwroot/assets/js/profile/profile-edit.js
@@ -112,7 +112,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                 await FYSCloud.API.uploadFile(newProfilePicUrl, newProfilePicDataUrl.url, true);
 
                 // If host is one of the fys clouds use that hosts url. Otherwise fall back on the mockup
-                const hostName = (new URL(window.location).hostname.includes('fys.cloud')) ? new URL(window.location).hostname : 'https://mockup-is109-4.fys.cloud';
+                const hostName = (new URL(window.location).hostname.includes('is109')) ? new URL(window.location).hostname : 'https://mockup-is109-4.fys.cloud';
                 try {
                     await FYSCloud.API.queryDatabase("UPDATE account SET name = ?, birthdate = ?, location = ?, phonenumber = ?, email = ?, bio = ?, profilePhoto = ?, genderFk = ? WHERE id = ?",
                         [newName, newBirthdate, newLocation, newPhoneNumber, newEmail, newBio, hostName + '/uploads/' + newProfilePicUrl, newGender, userId]);
