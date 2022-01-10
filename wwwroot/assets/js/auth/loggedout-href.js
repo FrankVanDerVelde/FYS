a3458c85636ddda0d7e7ed881251a45e65fd8d28
Harmohat <harmohat.khangura@hva.nl>
2022-01-10 17:17:42 +0100

    loggedout-href code changed to executing immediately instead of page loading

--- wwwroot/assets/js/auth/loggedout-href.js
+++ wwwroot/assets/js/auth/loggedout-href.js
@@ -1,8 +1,7 @@
 //voer het script uit als de site is geladen.
-window.addEventListener("load", function() {
-    let loggedin = FYSCloud.Session.get("loggedin");
 
-    if (!loggedin) {
-        window.location.replace("../views/auth/login.html");
-    }
-});
+let loggedin = FYSCloud.Session.get("loggedin");
+
+if (!loggedin) {
+    window.location.replace("../views/auth/login.html");
+}
