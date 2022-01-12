98db52a2e10b0d04f80a356c37b7cf0b473a8083
VEENMAM3 <VEENMAM3@ad.hva.nl>
2022-01-12 14:08:02 +0100

    Err msg panel.html

--- wwwroot/assets/js/admin/panel.js
+++ wwwroot/assets/js/admin/panel.js
@@ -6,25 +6,35 @@ async function searchUser() {
         await getUserByName(searchParam)
     ];
 
-    for(let i=0; i<data.length; i++) {
-        if(data[i] != null) {
-            await updatePanel(data[i]);
+
+
+    if (data[0] == undefined && data[1] == undefined) {
+        document.getElementById("name").innerText = "ERROR: Can't Find user!";
+        document.getElementById("email").innerText = "Did you type in the exact";
+    } else {
+        for (let i = 0; i < data.length; i++) {
+            if(data[i] != undefined) {
+                
+                await updatePanel(data[i]);
+            }
         }
     }
 }
+
+
 
 async function updatePanel(userObj) {
     document.getElementById("name").innerText = userObj.name;
     document.getElementById("email").innerText = userObj.email;
     document.getElementById("userImage").src = userObj.profilePhoto;
 
-    if(userObj.lastLoggedIn != null) {
+    if (userObj.lastLoggedIn != null) {
         document.getElementById("lastActive").innerText = await stripTimeFromDate(userObj.lastLoggedIn);
     }
 
-    // To be implemented
     document.getElementById("matchAmount").innerText = await getMatchAmount(userObj.id);
 }
+
 
 async function deleteCurrentUser() {
     const user = await getUserByEmailAsync(document.getElementById("email").innerText);
@@ -38,10 +48,10 @@ async function deleteCurrentUser() {
 
 async function getMatchAmount(userId) {
     const matchArray = await sqlSelectAsync("matches", ["currUserFk "], [userId]);
-    let amount= 0;
+    let amount = 0;
 
-    for(let i=0; i<matchArray.length; i++) {
-        if(matchArray[i].status === "accepted") {
+    for (let i = 0; i < matchArray.length; i++) {
+        if (matchArray[i].status === "accepted") {
             amount += 1;
         }
     }
